d9b7fae86db4e3eff975343c9d2894ea2d3570eb
Markus <markus.s.englund@gmail.com>
2018-05-06 01:12:39 +0200

    add preact-router

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,9 +1,24 @@
 import { h } from "preact";
+import Router, { Link } from "preact-router";
 import "./App.scss";
 
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <Link href="/about">About</Link>
+  </div>
+);
+
+const About = () => <h1>About</h1>;
+
 const App = () => {
-  const asdf = "asdf";
-  return <h1>{asdf}</h1>;
+  const about = "/about";
+  return (
+    <Router>
+      <Home path="/" />
+      <About path={about} />
+    </Router>
+  );
 };
 
 export default App;
